aafdc1185a86bc6fd0905936af57e329672b25f3
Charlotte <Charlotte@ZachCs-MacBook-Pro.local>
2022-06-17 12:28:21 -0400

    Initial commit

---
+++ button_increment/button_incrementApp.swift
@@ -0,0 +1,18 @@
+//
+//  button_incrementApp.swift
+//  button_increment
+//
+//  Created by Charlotte on 6/17/22.
+//
+//
+
+import SwiftUI
+
+@main
+struct button_incrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
